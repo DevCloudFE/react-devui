a9bd725d92848dc40cba0cd94c07e1548497ab66
Kagol <kagol@sina.com>
2022-08-10 14:46:16 +0800

    chore: add baidu statistic script

--- packages/site/src/main.tsx
+++ packages/site/src/main.tsx
@@ -4,8 +4,11 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './app/App';
 import './app/i18n';
+import { insertBaiduScript } from './utils';
 
 const rootElement = document.getElementById('app-root') as Element;
+
+insertBaiduScript();
 
 ReactDOMClient.createRoot(rootElement).render(
   <StrictMode>
