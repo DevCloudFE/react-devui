168176b865f13eca3429728e9a43496f23ac037a
xiejay97 <xiejay97@gmail.com>
2022-11-24 18:53:43 +0800

    chore(site): update styles

--- packages/site/src/app/routes/home/Home.tsx
+++ packages/site/src/app/routes/home/Home.tsx
@@ -17,15 +17,17 @@ export default function Home(): JSX.Element | null {
 
   return (
     <main className="app-home-route">
-      <Bg className="app-home-route__bg" />
-      <div className="app-home-route__title">React DevUI</div>
-      <div className="app-home-route__description">{t('home.Title')}</div>
-      <Link className="app-home-route__button-link" to="/docs/GettingStarted">
-        <DButton dIcon={<ArrowRightOutlined />} dIconRight>
-          {t('home.Getting Started')}
-        </DButton>
-      </Link>
-      <AppFooter style={{ flexGrow: 1, margin: '40px -20px -20px' }} />
+      <section className="app-home-route__page">
+        <Bg className="app-home-route__bg" />
+        <div className="app-home-route__title">React DevUI</div>
+        <div className="app-home-route__description">{t('home.Title')}</div>
+        <Link className="app-home-route__button-link" to="/docs/GettingStarted">
+          <DButton dIcon={<ArrowRightOutlined />} dIconRight>
+            {t('home.Getting Started')}
+          </DButton>
+        </Link>
+      </section>
+      <AppFooter />
     </main>
   );
 }
