b38e139676be4e8a620f175ee945a86cdce2cfcc
xiejay97 <xiejay97@gmail.com>
2022-12-05 17:02:58 +0800

    chore(ui): export `abstract-control`

--- packages/ui/src/components/form/index.ts
+++ packages/ui/src/components/form/index.ts
@@ -4,6 +4,7 @@ export * from './FormGroup';
 
 export * from './hooks';
 
+export * from './abstract-control';
 export * from './form-control';
 export * from './form-group';
 export * from './validators';
