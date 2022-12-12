c40f68a7e55b3db1319e089d67384f676f263bdd
xiejay97 <xiejay97@gmail.com>
2022-12-12 16:03:24 +0800

    refactor(ui): use React's `useId`

--- packages/hooks/src/useId.ts
+++ packages/hooks/src/useId.ts
@@ -1,22 +1,5 @@
-import { useMemo } from 'react';
+import { useId as useIdByReact } from 'react';
 
-const STR = 'abcdefghijklmnopqrstuvwxyz';
-
-const ID = [-1];
 export function useId(): string {
-  return useMemo(() => {
-    const reduce = (index: number) => {
-      const charIndex = ID[index] ?? -1;
-      if (charIndex + 1 === STR.length) {
-        ID[index] = 0;
-        reduce(index + 1);
-      } else {
-        ID[index] = charIndex + 1;
-      }
-    };
-    reduce(0);
-
-    return `${useId.PREFIX}${ID.map((charIndex) => STR[charIndex]).join('')}`;
-  }, []);
+  return useIdByReact();
 }
-useId.PREFIX = 'ID';
