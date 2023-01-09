fbed91fa1d1e776196318e00320823ab82d4c304
xiejay97 <xiejay97@gmail.com>
2023-01-09 10:00:25 +0800

    feat(hooks): useResize add skipEmpty param

--- packages/hooks/src/useResize.ts
+++ packages/hooks/src/useResize.ts
@@ -2,7 +2,7 @@ import { isUndefined } from 'lodash';
 import { useEffect, useRef } from 'react';
 import { flushSync } from 'react-dom';
 
-export function useResize(target: React.RefObject<Element | null>, cb?: ResizeObserverCallback, disabled = false): void {
+export function useResize(target: React.RefObject<Element | null>, cb?: ResizeObserverCallback, disabled = false, skipEmpty = true): void {
   const dataRef = useRef<{
     prevBorderBoxSize?: ResizeObserverSize;
   }>({});
@@ -16,7 +16,7 @@ export function useResize(target: React.RefObject<Element | null>, cb?: ResizeOb
       const observer = new ResizeObserver((entries, observer) => {
         if (
           !isUndefined(dataRef.current.prevBorderBoxSize) &&
-          !(entries[0].borderBoxSize[0].blockSize === 0 && entries[0].borderBoxSize[0].inlineSize === 0) &&
+          !(skipEmpty && entries[0].borderBoxSize[0].blockSize === 0 && entries[0].borderBoxSize[0].inlineSize === 0) &&
           (dataRef.current.prevBorderBoxSize.blockSize !== entries[0].borderBoxSize[0].blockSize ||
             dataRef.current.prevBorderBoxSize.inlineSize !== entries[0].borderBoxSize[0].inlineSize)
         ) {
