2ec4ed9fcc09b3f44c6c33a44e81ba644b16aeae
肖克松 <1141778924@qq.com>
2023-01-11 20:17:49 +0800

    test(ui): add tmp test of button

---
+++ packages/ui/src/components/button/Button.test.tsx
@@ -0,0 +1,11 @@
+import { render } from '../../__tests__/utils';
+import { DButton } from './index';
+
+describe('DButton', () => {
+  const text = 'This is DButton';
+
+  it('should `children` work', () => {
+    const { getByText } = render(<DButton children={text} />);
+    expect(getByText(text)).toBeInTheDocument();
+  });
+});
